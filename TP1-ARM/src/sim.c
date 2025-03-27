31bf14f7763eb69fd3513802edcd41942ffafbad
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-03-27 19:31:45 -0300

    Update sim.c

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -55,7 +55,7 @@ void init_instruction_table() {
     ADD_INSTRUCTION( 0xB1, split_i, adds_immediate, "Adds Immediate" );
 
     //ADD_INSTRUCTION( 0xB1, split_i, subs_extended_register, "Subs Extended Register" );
-    //ADD_INSTRUCTION( 0xB1, split_i, subs_immediate, "Subs Immediate" );
+    // ADD_INSTRUCTION( 0xB1, split_i, subs_immediate, "Subs Immediate" );
     //ADD_INSTRUCTION( 0x6a2, split_i, hlt, "HLT" );
 
     // ADD_INSTRUCTION( 0xB1, split_i, adds_immediate, "Adds Immediate" );
@@ -68,22 +68,28 @@ void init_instruction_table() {
 
 void adds_extended_register( partition_t *split_data ) {
     uint64_t operation = CURRENT_STATE.REGS[split_data->rn] + CURRENT_STATE.REGS[split_data->rm];
-    NEXT_STATE.FLAG_N = (operation < 0);
-    NEXT_STATE.FLAG_Z = (operation == 0);
+    NEXT_STATE.FLAG_N = operation >> 63;
+    NEXT_STATE.FLAG_Z = operation == 0;
+    if (split_data->rd != XZR) {
+        NEXT_STATE.REGS[split_data->rd] = operation;
+    }
 }
 
 void adds_immediate(partition_t *instruction_data) {
+    uint64_t operand1 = CURRENT_STATE.REGS[instruction_data->rn];
     uint64_t imm = instruction_data->alu;
     if (instruction_data->shamt == 0x1) {
         imm <<= 12;
     }
-    uint64_t operation = CURRENT_STATE.REGS[instruction_data->rn] + imm;
-    NEXT_STATE.FLAG_N = (operation < 0);
-    NEXT_STATE.FLAG_Z = (operation == 0);
+    uint64_t result = operand1 + imm;
+    NEXT_STATE.FLAG_N = result >> 63;
+    NEXT_STATE.FLAG_Z = result == 0;
+    if (instruction_data->rd != XZR) {
+        NEXT_STATE.REGS[instruction_data->rd] = result;
+    }
 }
 
 void subs_extended_register(partition_t *instruction_data) {
-
 }
 
 
