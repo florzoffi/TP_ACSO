fbcb11c9085717986510bc51703e9bfe0452843a
florzoffi <fzoffi@udesa.edu.ar>
2025-03-26 22:38:32 -0300

    delete sim.h and update decoder.h

--- TP1-ARM/src/decoder.h
+++ TP1-ARM/src/decoder.h
@@ -3,6 +3,16 @@
 
 #include <stdint.h>
 
+typedef struct {
+    void ( *decode )( partition_t *decoded, uint32_t instruction_code );
+    void ( *execute )( partition_t *decoded );
+    char *name;
+} instruction_info_t;
+
+void adds_extended_register(partition_t* p);
+void init_instruction_table();
+char* uint32_to_string( uint32_t number );
+
 typedef struct{
     uint32_t opcode;
     uint32_t rm;
