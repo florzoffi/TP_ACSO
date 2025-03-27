15dbfe6a52ad07b345fa784624726caa63bf366d
florzoffi <fzoffi@udesa.edu.ar>
2025-03-26 22:40:15 -0300

    reorder decoder.h

--- TP1-ARM/src/decoder.h
+++ TP1-ARM/src/decoder.h
@@ -3,16 +3,6 @@
 
 #include <stdint.h>
 
-typedef struct {
-    void ( *decode )( partition_t *decoded, uint32_t instruction_code );
-    void ( *execute )( partition_t *decoded );
-    char *name;
-} instruction_info_t;
-
-void adds_extended_register(partition_t* p);
-void init_instruction_table();
-char* uint32_to_string( uint32_t number );
-
 typedef struct{
     uint32_t opcode;
     uint32_t rm;
@@ -27,6 +17,16 @@ typedef struct{
     uint32_t cond_br;
     uint32_t mov;
 }partition_t;
+
+typedef struct {
+    void ( *decode )( partition_t *decoded, uint32_t instruction_code );
+    void ( *execute )( partition_t *decoded );
+    char *name;
+} instruction_info_t;
+
+void adds_extended_register(partition_t* p);
+void init_instruction_table();
+char* uint32_to_string( uint32_t number );
 
 void decode(void);
 void split_r(uint32_t instruction, partition_t* parts);
