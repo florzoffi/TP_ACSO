86e3e0035f798e4ccfd32a484529991415301d11
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-03-28 00:03:38 -0300

    Update sim.c

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -77,8 +77,8 @@ int32_t adjust_sign(uint32_t value, int bits) {
 
 void adds_extended_register( partition_t *split_data ) {
     uint64_t operation = CURRENT_STATE.REGS[split_data->rn] + CURRENT_STATE.REGS[split_data->rm];
-    NEXT_STATE.FLAG_Z = (operation == 0);
-    NEXT_STATE.FLAG_N = (operation < 0);
+    NEXT_STATE.FLAG_N = operation >> 63;
+    NEXT_STATE.FLAG_Z = operation == 0;
     NEXT_STATE.REGS[split_data->rd] = operation;
 }
 
@@ -96,7 +96,7 @@ void adds_immediate(partition_t *split_data) {
 
 void subs_extended_register(partition_t *split_data) {
     uint64_t operation = CURRENT_STATE.REGS[split_data->rn] - CURRENT_STATE.REGS[split_data->rm];
-    NEXT_STATE.FLAG_N = (operation < 0);
+    NEXT_STATE.FLAG_N = (operation >> 63) & 1;
     NEXT_STATE.FLAG_Z = (operation == 0);
     NEXT_STATE.REGS[split_data->rd] = operation;
     if (split_data->rd != 31) {
@@ -110,7 +110,7 @@ void subs_immediate(partition_t *split_data) {
         imm <<= 12;
     }
     uint64_t operation = CURRENT_STATE.REGS[split_data->rn] - imm;
-    NEXT_STATE.FLAG_N = (operation < 0);
+    NEXT_STATE.FLAG_N = (operation >> 63) & 1;
     NEXT_STATE.FLAG_Z = (operation == 0);
     NEXT_STATE.REGS[split_data->rd] = operation;
     if (split_data->rd != 31) {
@@ -123,26 +123,10 @@ void hlt(partition_t *split_data) {
 }
 
 void ands_shifted_register(partition_t *split_data) {
-    // Obtiene los valores de los registros basados en los índices especificados
-    uint64_t operand1 = CURRENT_STATE.REGS[split_data->rn];
-    uint64_t operand2 = CURRENT_STATE.REGS[split_data->rm];
-
-    // Aplica el desplazamiento especificado por shamt al segundo operando
-    operand2 <<= split_data->shamt;
-
-    // Realiza la operación AND bit a bit
-    uint64_t result = operand1 & operand2;
-
-    // Almacena el resultado en el registro especificado por rd, excepto si rd es un registro especial como XZR
-    if (split_data->rd != 31) {
-        NEXT_STATE.REGS[split_data->rd] = result;
-    }
-
-    // Actualiza las banderas de estado
-    NEXT_STATE.FLAG_N = (result >> 63) & 1;  // El bit más significativo indica el signo
-    NEXT_STATE.FLAG_Z = (result == 0);       // True si el resultado es cero
+    uint64_t operation = CURRENT_STATE.REGS[split_data->rn] & CURRENT_STATE.REGS[split_data->rm];
+    NEXT_STATE.FLAG_N = operation >> 63;
+    NEXT_STATE.FLAG_Z = operation == 0;
 }
-
 
 void eor_shifted_register(partition_t *split_data) {
     uint64_t operation = CURRENT_STATE.REGS[split_data->rn] ^ CURRENT_STATE.REGS[split_data->rm];
