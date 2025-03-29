ee790bd70817f773c42f280531bd396a073b0b25
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-03-29 00:13:45 -0300

    update br.s

--- TP1-ARM/inputs/br.s
+++ TP1-ARM/inputs/br.s
@@ -1,6 +1,7 @@
 .text
-movz x1, 0x0008         // br a 0x00400008
-br x1
-movz x0, 0xFFFF         // no se ejecuta
-movz x2, 0xABCD         // instrucción en 0x00400008
+MOVZ X0, #0x1000
+MOVZ X1, #5           
+BR X0
+MOVZ X1, #10
+MOVZ X2, #1
 HLT 0
