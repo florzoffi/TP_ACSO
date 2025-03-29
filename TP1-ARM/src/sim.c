7e6e75b49615958778264bb2fd46e749d77f12c4
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-03-28 23:56:54 -0300

    Update sim.c

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -114,7 +114,7 @@ void subs_extended_register(partition_t *split_data) {
     NEXT_STATE.FLAG_Z = (operation == 0);
     NEXT_STATE.REGS[split_data->rd] = operation;
     //CMP
-    if (split_data->rd != 32) {
+    if (split_data->rd != 31) {
         NEXT_STATE.REGS[split_data->rd] = operation;
     }
 }
@@ -129,7 +129,7 @@ void subs_immediate(partition_t *split_data) {
     NEXT_STATE.FLAG_Z = (operation == 0);
     NEXT_STATE.REGS[split_data->rd] = operation;
     //CMP
-    if (split_data->rd != 32) {
+    if (split_data->rd != 31) {
         NEXT_STATE.REGS[split_data->rd] = operation;
     }
 }
