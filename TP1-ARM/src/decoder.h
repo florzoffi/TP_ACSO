6119d58467f47a184a8d6b20a0ef2d8d59310d3f
florzoffi <fzoffi@udesa.edu.ar>
2025-03-30 17:42:27 -0300

    compilation error

--- TP1-ARM/src/decoder.h
+++ TP1-ARM/src/decoder.h
@@ -24,34 +24,6 @@ typedef struct {
     char *name;
 } instruction_info_t;
 
-
-void adds_extended_register(partition_t* p);
-void adds_immediate(partition_t* p);
-void subs_extended_register(partition_t* p);
-void subs_immediate(partition_t* p);
-void hlt(partition_t* p);
-void ands_shifted_register(partition_t* p);
-void eor_shifted_register(partition_t* p);
-void orr_shifted_register(partition_t* p);
-void b(partition_t* p);
-void br_register(partition_t* p);
-void b_cond(partition_t* p);
-void lsl_lsr_immediate(partition_t* p);
-
-void stur(partition_t* p);
-void sturb(partition_t* p);
-void sturh(partition_t* p);
-void ldur(partition_t* p);
-void ldurb(partition_t* p);
-void ldurh(partition_t* p);
-void movz(partition_t* p);
-
-void add_immediate(partition_t* p);
-void add_extended_register(partition_t* p);
-void mul_register(partition_t* p);
-void cbz(partition_t* p);
-void cbnz(partition_t* p);
-
 void init_instruction_table();
 char* uint32_to_string( uint32_t number );
 
--- TP1-ARM/src/functions.h
+++ TP1-ARM/src/functions.h
@@ -1,15 +1,7 @@
-#include <stdio.h>
-#include <assert.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <inttypes.h>
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 
-#include "shell.h"
-#include "hopscotch.h"
 #include "decoder.h"
-#include "sim.c"
 
 void adds_extended_register( partition_t* p );
 void adds_immediate( partition_t* p );
@@ -23,3 +15,17 @@ void b( partition_t* p );
 void br_register( partition_t* p );
 void b_cond( partition_t* p );
 void lsl_lsr_immediate( partition_t* p );
+void stur( partition_t* p );
+void sturb( partition_t* p );
+void sturh( partition_t* p );
+void ldur( partition_t* p );
+void ldurb( partition_t* p );
+void ldurh( partition_t* p );
+void movz( partition_t* p );
+void add_immediate( partition_t* p );
+void add_extended_register( partition_t* p );
+void mul_register( partition_t* p );
+void cbz( partition_t* p );
+void cbnz( partition_t* p );
+
+#endif
