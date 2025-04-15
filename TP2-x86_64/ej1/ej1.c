f45fc059de92cb2ddcbc9b94c9be885572321076
florzoffi <fzoffi@udesa.edu.ar>
2025-04-15 19:50:26 -0300

    delete NULL didnt work

--- TP2-x86_64/ej1/ej1.c
+++ TP2-x86_64/ej1/ej1.c
@@ -2,7 +2,7 @@
 
 string_proc_list* string_proc_list_create(void){
 	string_proc_list* list = ( string_proc_list* )malloc( sizeof( string_proc_list ) );
-    if ( !list ) return;
+    if ( !list ) return NULL;
     list->first = NULL;
     list->last = NULL;
     return list;
@@ -10,7 +10,7 @@ string_proc_list* string_proc_list_create(void){
 
 string_proc_node* string_proc_node_create( uint8_t type, char* hash ){
 	string_proc_node* node = ( string_proc_node* )malloc( sizeof( string_proc_node ) );
-    if ( !node ) return;
+    if ( !node ) return NULL;
     node->type = type;
     node->hash = hash;        
     node->next = NULL;
