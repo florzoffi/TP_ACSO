e82a167911cff5209816fa04bf8f1be04afe3225
florzoffi <fzoffi@udesa.edu.ar>
2025-05-13 18:56:38 -0300

    Update inode.c

--- TP3-FileSystem/inode.c
+++ TP3-FileSystem/inode.c
@@ -9,7 +9,7 @@
 #define INODES_PER_BLOCK (DISKIMG_SECTOR_SIZE / sizeof(struct inode))
 
 /**
- * TODO
+ * Lee un inodo del disco, dado su número (inumber) y lo guarda en *inp.
  */
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
     if (inumber < 1 || inumber > fs->superblock.s_isize * INODES_PER_BLOCK) {
@@ -31,63 +31,63 @@ int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
 }
 
 /**
- * TODO
+ * Devuelve el número de bloque físico del bloque lógico blockNum de un archivo.
  */
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {  
     if ((inp->i_mode & IALLOC) == 0) {
         return -1;
     }
-
     if ((inp->i_mode & ILARG) == 0) {
         if (blockNum < 0 || blockNum >= 8) {
             return -1;
         }
         return inp->i_addr[blockNum];
     }
+    int ptrsPerBlock = DISKIMG_SECTOR_SIZE / sizeof(unsigned int);
+    if (blockNum < 7 * ptrsPerBlock) {
+        int indBlockIdx = blockNum / ptrsPerBlock;
+        int offset = blockNum % ptrsPerBlock;
 
-    int indirectPtrsPerBlock = DISKIMG_SECTOR_SIZE / sizeof(unsigned int);
-
-    if (blockNum < 7 * indirectPtrsPerBlock) {
-        int indirectBlockIndex = blockNum / indirectPtrsPerBlock;
-        int offset = blockNum % indirectPtrsPerBlock;
-
-        if (indirectBlockIndex >= 7) {
+        if (indBlockIdx >= 7) {
             return -1;
         }
 
-        unsigned int pointers[indirectPtrsPerBlock];
-        int bytesRead = diskimg_readsector(fs->dfd, inp->i_addr[indirectBlockIndex], pointers);
+        unsigned int indirect[ptrsPerBlock];
+        int bytesRead = diskimg_readsector(fs->dfd, inp->i_addr[indBlockIdx], indirect);
         if (bytesRead == -1) {
             return -1;
         }
 
-        return pointers[offset];
+        return indirect[offset];
     }
 
-    int remaining = blockNum - 7 * indirectPtrsPerBlock;
+    int remaining = blockNum - 7 * ptrsPerBlock;
 
-    if (remaining >= indirectPtrsPerBlock * indirectPtrsPerBlock) {
+    if (remaining >= ptrsPerBlock * ptrsPerBlock) {
         return -1;
     }
 
-    int first = remaining / indirectPtrsPerBlock;
-    int second = remaining % indirectPtrsPerBlock;
+    int outer = remaining / ptrsPerBlock;
+    int inner = remaining % ptrsPerBlock;
 
-    unsigned int firstLevel[indirectPtrsPerBlock];
+    unsigned int firstLevel[ptrsPerBlock];
     int bytesRead = diskimg_readsector(fs->dfd, inp->i_addr[8], firstLevel);
     if (bytesRead == -1) {
         return -1;
     }
 
-    unsigned int secondLevel[indirectPtrsPerBlock];
-    bytesRead = diskimg_readsector(fs->dfd, firstLevel[first], secondLevel);
+    unsigned int secondLevel[ptrsPerBlock];
+    bytesRead = diskimg_readsector(fs->dfd, firstLevel[outer], secondLevel);
     if (bytesRead == -1) {
         return -1;
     }
 
-    return secondLevel[second];
+    return secondLevel[inner];
 }
 
+/**
+ * Devuelve el tamaño del archivo en bytes a partir de i_size0 e i_size1.
+ */
 int inode_getsize(struct inode *inp) {
-  return ((inp->i_size0 << 16) | inp->i_size1); 
+    return ((inp->i_size0 << 16) | inp->i_size1); 
 }
