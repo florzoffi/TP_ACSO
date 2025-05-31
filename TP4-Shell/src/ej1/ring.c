5de18bde29a33d4e77f77a07c096d66c58720ee0
florzoffi <fzoffi@udesa.edu.ar>
2025-05-31 12:47:55 -0300

    entrega

--- TP4-Shell/src/ej1/ring.c
+++ TP4-Shell/src/ej1/ring.c
@@ -3,91 +3,60 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(int argc, char **argv) {
-    if (argc != 4) {
-        fprintf(stderr, "Uso: %s <n> <c> <s>\n", argv[0]);
-        exit(EXIT_FAILURE);
+int main( int argc, char **argv ) {
+    if ( argc != 4 ) {
+        fprintf( stderr, "Uso: %s <n> <c> <s>\n", argv[0] );
+        exit( EXIT_FAILURE );
     }
-
-    int n = atoi(argv[1]);
-    int valor_inicial = atoi(argv[2]);
-    int start = atoi(argv[3]);
-
-    if (n < 3) {
-        fprintf(stderr, "Error: debe haber al menos 3 procesos para formar un anillo\n");
-        exit(1);
-    }
-
-    if (start >= n) {
-        printf("Indice de inicio fuera de rango\n");
-        exit(1);
-    }
-
-    if (start < 0) {
-        printf("Indice de inicio negativo\n");
-        exit(1);
-    }
-
+    int n = atoi( argv[1] );
+    int valor_inicial = atoi( argv[2] );
+    int start = atoi( argv[3] );
+    if ( n < 3 ) { exit( 1 ); }
+    if ( start >= n ) { exit( 1 ); }
+    if ( start < 0 ) { exit( 1 ); }
     int pipes[n][2];
-    for (int i = 0; i < n; i++) {
-        if (pipe(pipes[i]) == -1) {
-            perror("pipe");
-            exit(1);
+    for ( int i = 0; i < n; i++ ) {
+        if ( pipe( pipes[i] ) == -1 ) {
+            perror( "pipe" );
+            exit( 1 );
         }
     }
-
     int pipe_final[2];
-    if (pipe(pipe_final) == -1) {
-        perror("pipe final");
-        exit(1);
+    if ( pipe( pipe_final ) == -1 ) {
+        perror( "pipe final" );
+        exit( 1 );
     }
-
-    for (int i = 0; i < n; i++) {
+    for ( int i = 0; i < n; i++ ) {
         pid_t pid = fork();
-        if (pid == -1) {
-            perror("fork");
-            exit(1);
+        if ( pid == -1 ) {
+            perror( "fork" );
+            exit( 1 );
         }
-
-        if (pid == 0) {
-            for (int j = 0; j < n; j++) {
-                if (j != i) close(pipes[j][0]);
-                if (j != (i + 1) % n) close(pipes[j][1]);
+        if ( pid == 0 ) {
+            for ( int j = 0; j < n; j++ ) {
+                if ( j != i ) close( pipes[j][0] );
+                if ( j != ( i + 1 ) % n ) close( pipes[j][1] );
             }
-            close(pipe_final[0]);
-
+            close( pipe_final[0] );
             int valor;
-            read(pipes[i][0], &valor, sizeof(int));
-
+            read( pipes[i][0], &valor, sizeof( int ) );
             valor++;
-            if (i == (start + n - 1) % n) {
-                write(pipe_final[1], &valor, sizeof(int));
-            } else {
-                write(pipes[(i + 1) % n][1], &valor, sizeof(int));
-            }
-
-            exit(0);
+            if ( i == ( start + n - 1 ) % n ) { write( pipe_final[1], &valor, sizeof( int ) ); }
+            else { write( pipes[( i + 1 ) % n][1], &valor, sizeof( int ) ); }
+            exit( 0 );
         }
     }
-
-    for (int i = 0; i < n; i++) {
-        if (i != start) close(pipes[i][1]);
-        close(pipes[i][0]);
+    for ( int i = 0; i < n; i++ ) {
+        if ( i != start ) close( pipes[i][1] );
+        close( pipes[i][0] );
     }
-    close(pipe_final[1]);
-
-    write(pipes[start][1], &valor_inicial, sizeof(int));
-    close(pipes[start][1]);
-
+    close( pipe_final[1] );
+    write( pipes[start][1], &valor_inicial, sizeof( int ) );
+    close( pipes[start][1] );
     int resultado_final;
-    read(pipe_final[0], &resultado_final, sizeof(int));
-    close(pipe_final[0]);
-
-    printf("Resultado final: %d\n", resultado_final);
-
-    for (int i = 0; i < n; i++) {
-        wait(NULL);
-    }
-
+    read( pipe_final[0], &resultado_final, sizeof( int ) );
+    close( pipe_final[0] );
+    printf( "El resultado es: %d\n", resultado_final);
+    for ( int i = 0; i < n; i++ ) { wait( NULL ); }
     return 0;
 }
